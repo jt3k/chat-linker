c82651bd79a651b741df1e0b15920f66f1ac8d9b
Andrey Gurtovoy <iyntx@ya.ru>
2016-12-07 14:06:54 +0300

    Update prevent-sleep.js

--- prevent-sleep.js
+++ prevent-sleep.js
@@ -9,5 +9,5 @@ const handleRequest = (request, response) => {
 };
 http.createServer(handleRequest).listen(process.env.PORT || 5000);
 setInterval(() => {
-  http.get('https://chat-linker.herokuapp.com');
+  http.get('https://translate.google.ru/translate?sl=en&tl=ru&js=y&prev=_t&hl=ru&ie=UTF-8&u=https%3A%2F%2Fchat-linker.herokuapp.com');
 }, 300000);
