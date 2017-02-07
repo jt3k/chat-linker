e38ea64dd0cb2ee89eeb48c06cb8ec0de420099a
Andrey Gurtovoy <iyntx@ya.ru>
2017-02-07 11:00:24 +0300

    add visible the chat messages in the terminal only in dev-mode
    #27

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
 
 const bus = require('./bus.js');
 
+const __DEV__ = process.env.NODE_ENV === 'dev';
+
 // global for debugging
 const jabber = global.jabber = require('./bots/jabber');
 const telegram = global.telegram = require('./bots/telegram');
 
 bus.on('message', ({network, room, name, message}) => {
-  console.log(`${network} "${room}": <${name}> ${message}`);
+  if (__DEV__) {
+    console.log(`${network} "${room}": <${name}> ${message}`);
+  }
 
   if (network === 'JABBER') {
     telegram.send({name, message});
